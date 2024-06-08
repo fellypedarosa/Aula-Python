ba77fdcd4d52954e5274b84667f7ed1b89331b73
Fellype Rosa <fellypedarosa@icloud.com>
2024-06-07 21:36:41 -0300

    Delete Aula#8 directory

--- Aula#8/emoji.py
+++
@@ -1,5 +0,0 @@
-import emoji
-print(emoji.emojize('Olá mundo :sunglasses:', language='alias'))
-print(emoji.emojize('Olá mundo :sunglasses:'))
-print(emoji.emojize('Olá mundo :red_heart:'))
-print(emoji.emojize('Olá mundo :morcego:', language='pt'))
